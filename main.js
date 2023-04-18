52d53b070f3dbc651c20014bda1ed272c17721f6
Tiffany Bissonnette <tiffanybissonnette@Tiffanys-MacBook-Air.local>
2023-04-17 22:39:22 -0400

    doneisdone

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 // API key
-const api = "19975f0371f4997472de6a125c67a809";
+const api = "19975f0371f4997472*****";
 
-// const iconImg = document.getElementById('weather-icon');
-// const loc = document.querySelector('#location');
-// const tempC = document.querySelector('.c');
-// const tempF = document.querySelector('.f');
-// const feels = document.querySelector('.feels')
-// const desc = document.querySelector('.desc');
-// const sunriseDOM = document.querySelector('.sunrise');
-// const sunsetDOM = document.querySelector('.sunset');
 
 const geo = async (city_name) => {
     const resp = await axios.get(
@@ -18,71 +10,25 @@ const geo = async (city_name) => {
         return resp.data;
     }
 };
+
 const inputField = document.getElementById("cityInput");
 const form = document.getElementById("cityForm");
-// form.addEventListener('submit', formSubmit);
 
-// window.addEventListener('load', () => {
-//   while (!formSubmit){
-
-//   if (navigator.geolocation) {
-//     navigator.geolocation.getCurrentPosition((position) => {
-//       // Storing Longitude and Latitude in variables
-//       let long = position.coords.longitude;
-//       let lat = position.coords.latitude;
-//       const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
-
-//       fetch(base)
-//         .then((response) => {
-//           return response.json();
-//         })
-//         //getting data
-//         .then((data) => {
-//           const { temp, feels_like } = data.main;
-//           const place = data.name;
-//           const { main, icon } = data.weather[0];
-//           const { sunrise, sunset } = data.sys;
-
-//           const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-//           const fahrenheit = (temp * 9) / 5 + 32;
-//           const f_fahrenheit = (feels_like * 9) / 5 + 32;
-//           const sunriseGMT = new Date(sunrise * 1000);
-//           const sunsetGMT = new Date(sunset * 1000);
-
-//           // displaying data on page
-//           iconImg.src = iconUrl;
-//           loc.textContent = `${place}`;
-//           desc.textContent = `${main}`;
-//           tempC.textContent = `${temp.toFixed(0)} °C`;
-//           tempF.textContent = `${fahrenheit.toFixed(0)} °F`;
-//           feels.textContent = `${feels_like.toFixed(0)} °C or ${f_fahrenheit.toFixed(0)} °F`;
-//           sunriseDOM.textContent = `${sunriseGMT.toLocaleTimeString()}`;
-//           sunsetDOM.textContent = `${sunsetGMT.toLocaleTimeString()}`;
-
-//           //   if(desc=='Clouds'){
-//           //     document.body.style.backgroundImage =url('../images/clouds.jpeg')
-//           //   };
-//         });
-//       ;
-//     }
-//     )
-//   }
-// };
+//when form is used
 let formSubmit = async (event) => {
     event.preventDefault();
     let city_name = inputField.value;
 
     let geo_info = await geo(city_name);
-    console.log(geo_info);
-    // navigator.geolocation.getElementById(city_name);
     let lat = geo_info[0]["lat"];
     let long = geo_info[0]["lon"];
     const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=imperial`;
-
+  //get info from api
     fetch(base)
         .then((response) => {
             return response.json();
         })
+        //display info
         .then((data) => {
             document.getElementById("place").innerHTML = data.name;
             const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
@@ -91,6 +37,7 @@ let formSubmit = async (event) => {
             document.getElementById("feels").innerHTML = `Feels like ${data.main.feels_like.toFixed(0)}°F`;
             document.getElementById("desc").innerHTML = data.weather[0].main;
 
+            //interative background
             const bg = document.querySelector("#background");
             const weather = data.weather[0].main.toLowerCase();
             if (weather == "clouds") {
@@ -110,6 +57,8 @@ let formSubmit = async (event) => {
             }
         });
 };
+
+//form event listener
 form.addEventListener("submit", formSubmit);
 
  
