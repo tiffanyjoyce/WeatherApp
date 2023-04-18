020bbe2d24af341cb3d74f165678803ec460836d
Tiffany Bissonnette <tiffanybissonnette@Tiffanys-MacBook-Air.local>
2023-04-17 22:36:45 -0400

    done

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // API key
-const api = '19975f0371f4997472de6a125c67a809';
+const api = "19975f0371f4997472de6a125c67a809";
 
 // const iconImg = document.getElementById('weather-icon');
 // const loc = document.querySelector('#location');
@@ -11,13 +11,15 @@ const api = '19975f0371f4997472de6a125c67a809';
 // const sunsetDOM = document.querySelector('.sunset');
 
 const geo = async (city_name) => {
-  const resp = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${api}`);
-  if (resp.status == 200){
-      return resp.data
-  }
-}
-const inputField = document.getElementById('cityInput')
-const form = document.getElementById('cityForm');
+    const resp = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city_name}&appid=${api}`
+    );
+    if (resp.status == 200) {
+        return resp.data;
+    }
+};
+const inputField = document.getElementById("cityInput");
+const form = document.getElementById("cityForm");
 // form.addEventListener('submit', formSubmit);
 
 // window.addEventListener('load', () => {
@@ -29,7 +31,6 @@ const form = document.getElementById('cityForm');
 //       let long = position.coords.longitude;
 //       let lat = position.coords.latitude;
 //       const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
-
 
 //       fetch(base)
 //         .then((response) => {
@@ -58,7 +59,6 @@ const form = document.getElementById('cityForm');
 //           sunriseDOM.textContent = `${sunriseGMT.toLocaleTimeString()}`;
 //           sunsetDOM.textContent = `${sunsetGMT.toLocaleTimeString()}`;
 
-
 //           //   if(desc=='Clouds'){
 //           //     document.body.style.backgroundImage =url('../images/clouds.jpeg')
 //           //   };
@@ -69,68 +69,47 @@ const form = document.getElementById('cityForm');
 //   }
 // };
 let formSubmit = async (event) => {
-  event.preventDefault();
-  let city_name = inputField.value;
-  let geo_info = await geo(city_name);
-  // navigator.geolocation.getElementById(city_name);
-  let lat = geo_info['lat'];
-  let long = geo_info['lon'];
-  const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=imperial`;
+    event.preventDefault();
+    let city_name = inputField.value;
 
-  fetch(base)
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    document.getElementById('place').innerHTML = data.name;
-    document.getElementById('icon').innerHTML= data.weather[0].icon;
-    document.getElementById('temp').innerHTML= `${data.main.temp}°F`;
-    document.getElementById('feels').innerHTML= `Feels like ${data.main.feels_like}°F`;
-    document.getElementById('desc').innerHTML= data.weather[0].main;
+    let geo_info = await geo(city_name);
+    console.log(geo_info);
+    // navigator.geolocation.getElementById(city_name);
+    let lat = geo_info[0]["lat"];
+    let long = geo_info[0]["lon"];
+    const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=imperial`;
 
-    if (data.weather[0].main == 'Clouds'){
-      document.getElementById('background').innerHTML = `<img src='images/clouds.jpeg></img>`
-    } else if (data.weather[0].main == 'Rain'){
-      document.getElementById('background').innerHTML = `<img src='images/rain.webp></img>`
-    } else if (data.weather[0].main == 'Clear'){
-      document.getElementById('background').innerHTML = `<img src='images/clear.webp></img>`
-    } else if (data.weather[0].main == 'Drizzle'){
-      document.getElementById('background').innerHTML = `<img src='images/drizzle.jpeg></img>`
-    } else if (data.weather[0].main == 'Mist'){
-      document.getElementById('background').innerHTML = `<img src='images/mist.jpeg></img>`
-    } else if (data.weather[0].main == 'Snow'){
-      document.getElementById('background').innerHTML = `<img src='images/snow.jpeg></img>`
-    } else if (data.weather[0].main == 'Thunderstorm'){
-      document.getElementById('background').innerHTML = `<img src='images/thunderstorm.jpeg></img>`
-    };
-  
-    // form.reset();
-    // const { temp, feels_like } = data.main;
-    //       const place = data.name;
-    //       const { main, icon } = data.weather[0];
-    //       const { sunrise, sunset } = data.sys;
+    fetch(base)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            document.getElementById("place").innerHTML = data.name;
+            const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+            document.getElementById("icon").innerHTML = data.weather[0].icon;
+            document.getElementById("temp").innerHTML = `${data.main.temp.toFixed(0)}°F`;
+            document.getElementById("feels").innerHTML = `Feels like ${data.main.feels_like.toFixed(0)}°F`;
+            document.getElementById("desc").innerHTML = data.weather[0].main;
 
-    //       const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-    //       const fahrenheit = (temp * 9) / 5 + 32;
-    //       const f_fahrenheit = (feels_like * 9) / 5 + 32;
-    //       const sunriseGMT = new Date(sunrise * 1000);
-    //       const sunsetGMT = new Date(sunset * 1000);
-          
-    //       // displaying data on page
-    //       iconImg.src = iconUrl;
-    //       loc.textContent = `${place}`;
-    //       desc.textContent = `${main}`;
-    //       tempC.textContent = `${temp.toFixed(0)} °C`;
-    //       tempF.textContent = `${fahrenheit.toFixed(0)} °F`;
-    //       feels.textContent = `${feels_like.toFixed(0)} °C or ${f_fahrenheit.toFixed(0)} °F`;
-    //       sunriseDOM.textContent = `${sunriseGMT.toLocaleTimeString()}`;
-    //       sunsetDOM.textContent = `${sunsetGMT.toLocaleTimeString()}`;
-  }
-
-  )
-}
-// const inputField = document.getElementById('cityInput')
-// const form = document.getElementById('cityForm');
-form.addEventListener('click', formSubmit);
+            const bg = document.querySelector("#background");
+            const weather = data.weather[0].main.toLowerCase();
+            if (weather == "clouds") {
+                bg.setAttribute("data-bg", weather);
+            } else if (weather == "clear") {
+                bg.setAttribute("data-bg", weather);
+            } else if (weather == "drizzle") {
+                bg.setAttribute("data-bg", weather);
+            } else if (weather == "mist") {
+                bg.setAttribute("data-bg", weather);
+            } else if (weather == "rain") {
+                bg.setAttribute("data-bg", weather);
+            } else if (weather == "snow") {
+                bg.setAttribute("data-bg", weather);
+            } else {
+                bg.setAttribute("data-bg", weather);
+            }
+        });
+};
+form.addEventListener("submit", formSubmit);
 
  
